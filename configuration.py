488663d82ac6ed85d00aeb5a4f028c046fe4e06b
Veronica Torres <qaveronicatorres@gmail.com>
2026-04-16 08:49:50 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE =  "https://cnt-d00cf47f-70d4-478e-be3f-cc7ca329d696.containerhub.tripleten-services.com"
+DOC_PATH =  "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,17 @@
+from requests import post
+
+import configuration
+import requests
+import data
+
+def post_new_user(body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+response = post_new_user(data.user_body)
+print(response.status_code)
+print(response.json())
+
+
+
